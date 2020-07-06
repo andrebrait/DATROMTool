9c00a32370f91d4b7371d209509ec1ea754738d5
Andre Brait <andrebrait@gmail.com>
2020-07-06 20:54:22 +0200

    Remove safeguards and let tests fail

--- core/src/test/java/io/github/datromtool/util/XMLValidator.java
+++ core/src/test/java/io/github/datromtool/util/XMLValidator.java
@@ -2,7 +2,6 @@ package io.github.datromtool.util;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.xml.sax.SAXException;
 
 import javax.xml.XMLConstants;
 import javax.xml.transform.Source;
@@ -12,9 +11,7 @@ import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
-import java.io.IOException;
 import java.io.InputStream;
-import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -26,23 +23,19 @@ public final class XMLValidator {
 
     private final static Logger logger = LoggerFactory.getLogger(XMLValidator.class);
 
-    public static void validateDat(byte[] xml) throws SAXException, IOException {
+    public static void validateDat(byte[] xml) throws Exception {
         validateDat(xml, "xsd/datafile/datafile.xsd");
     }
 
-    public static void validateDetector(byte[] xml) throws SAXException, IOException {
+    public static void validateDetector(byte[] xml) throws Exception {
         validateDat(xml, "xsd/detector/detector.xsd");
     }
 
-    private static void validateDat(byte[] xml, String xsd) throws SAXException, IOException {
-        try {
-            validate(xml, Paths.get(ClassLoader.getSystemResource(xsd).toURI()));
-        } catch (URISyntaxException e) {
-            logger.error("Could not open XSD file", e);
-        }
+    private static void validateDat(byte[] xml, String xsd) throws Exception {
+        validate(xml, Paths.get(ClassLoader.getSystemResource(xsd).toURI()));
     }
 
-    private static void validate(byte[] xml, Path xsd) throws SAXException, IOException {
+    private static void validate(byte[] xml, Path xsd) throws Exception {
         // create a SchemaFactory capable of understanding WXS schemas
         SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
 
