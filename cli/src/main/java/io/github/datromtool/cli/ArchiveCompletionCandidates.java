16e116e8cc1b40bae66a7ca0652d8f61c45f9d77
Andre Brait <andrebrait@gmail.com>
2020-09-17 19:42:26 +0200

    Compute candidates on the fly

--- cli/src/main/java/io/github/datromtool/cli/ArchiveCompletionCandidates.java
+++ cli/src/main/java/io/github/datromtool/cli/ArchiveCompletionCandidates.java
@@ -1,6 +1,5 @@
 package io.github.datromtool.cli;
 
-import com.google.common.collect.ImmutableList;
 import io.github.datromtool.io.ArchiveType;
 
 import javax.annotation.Nonnull;
@@ -9,15 +8,12 @@ import java.util.Iterator;
 
 public final class ArchiveCompletionCandidates implements Iterable<String> {
 
-    private static final ImmutableList<String> OUTPUT_TYPES =
-            Arrays.stream(ArchiveType.values())
-                    .filter(ArchiveType::isAvailableAsOutput)
-                    .map(Enum::name)
-                    .collect(ImmutableList.toImmutableList());
-
     @Override
     @Nonnull
     public Iterator<String> iterator() {
-        return OUTPUT_TYPES.iterator();
+        return Arrays.stream(ArchiveType.values())
+                .filter(ArchiveType::isAvailableAsOutput)
+                .map(Enum::name)
+                .iterator();
     }
 }
